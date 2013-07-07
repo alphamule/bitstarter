eb36d4865fb2dbeb40655c18bac9758c438a7b2d
Eric Wagner <github@alphamule.com>
2013-07-06 23:01:16 -0700

    fixed grader.js to take urls

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://secret-springs-4578.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +38,16 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlText = function(htmltext) {
+    return cheerio.load(htmltext);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlText = function(htmltext, checksfile) {
+    $ = cheerioHtmlText(htmltext);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +63,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var handleResult = function(text, checks) {
+    var checkJson = checkHtmlText(text, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to check', null, null)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var html = '';
+    if (program.url) {
+        var response = restler.get(program.url).on('complete', function(data) {
+            html = data;
+            handleResult(html, program.checks);
+        });
+    } else {
+        html = fs.readFileSync(program.file);
+        handleResult(html, program.checks);
+    }
+
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlText = checkHtmlText;
 }
